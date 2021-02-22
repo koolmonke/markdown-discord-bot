a82a62e0a66d550185fc7c83e3cfa662f7b0fc01
Artur Khakimov <verybigfolder@yandex.ru>
2021-02-22 19:12:35 +0500

    Split message into chunks on files exciting discord message size limit

--- markdown_discord_bot/__init__.py
+++ markdown_discord_bot/__init__.py
@@ -6,6 +6,10 @@ from markdown_discord_bot import config
 client = discord.Client()
 
 
+def split_with_char_limit(string: str, max_size: int):
+    return ((string[i:i + max_size]) for i in range(0, len(string), max_size))
+
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
@@ -15,16 +19,13 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.attachments:
+    if message.attachments and message.content != "no-preview":
         ext = message.attachments[0].filename.split('.')[1]
         if ext in config.EXTS:
-            if (message.attachments[0].size + len(ext) + 8) <= 2000:  # 8 is len(```) x2+\n x2 in bytes
-                print(f'Got {message.attachments[0].filename}')
-                file_content = (await message.attachments[0].read()).decode()
-                await message.channel.send(f"```{ext}\n{file_content}\n```")
-            else:
-                await message.channel.send(
-                    "Слишком большой файл. Невозможно отправить в качестве одного сообщения")
+            print(f'Got {message.attachments[0].filename}')
+            file_content = (await message.attachments[0].read()).decode()
+            for part in split_with_char_limit(file_content, 1900):
+                await message.channel.send(f"```{ext}\n{part}\n```")
 
 
 def main():
