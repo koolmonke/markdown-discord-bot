f58d282fceeb4aabadc81663c3b386f02ddcdde0
Artur Khakimov <verybigfolder@yandex.ru>
2021-02-22 20:33:36 +0500

    Log user name too

--- markdown_discord_bot/__init__.py
+++ markdown_discord_bot/__init__.py
@@ -22,7 +22,7 @@ async def on_message(message):
     if message.attachments and message.content != "no-preview":
         ext = message.attachments[0].filename.split('.')[1]
         if ext in config.EXTS:
-            print(f"Got {message.attachments[0].filename}")
+            print(f"Got {message.attachments[0].filename} from {message.author}")
             file_content = (await message.attachments[0].read()).decode()
             for part in split_with_char_limit(file_content, 1900):
                 await message.channel.send(f"```{ext}\n{part}\n```")
