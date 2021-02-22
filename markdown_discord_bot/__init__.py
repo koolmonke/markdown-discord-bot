24f6b47a7b0bf4ec85e4797e9942e6fae3ce528a
Artur Khakimov <verybigfolder@yandex.ru>
2021-02-22 20:21:32 +0500

    Code reformat

--- markdown_discord_bot/__init__.py
+++ markdown_discord_bot/__init__.py
@@ -12,7 +12,7 @@ def split_with_char_limit(string: str, max_size: int):
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f"We have logged in as {client.user}")
 
 
 @client.event
@@ -22,7 +22,7 @@ async def on_message(message):
     if message.attachments and message.content != "no-preview":
         ext = message.attachments[0].filename.split('.')[1]
         if ext in config.EXTS:
-            print(f'Got {message.attachments[0].filename}')
+            print(f"Got {message.attachments[0].filename}")
             file_content = (await message.attachments[0].read()).decode()
             for part in split_with_char_limit(file_content, 1900):
                 await message.channel.send(f"```{ext}\n{part}\n```")
--- markdown_discord_bot/__main__.py
+++ markdown_discord_bot/__main__.py
@@ -1,4 +1,4 @@
 from markdown_discord_bot import main
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
