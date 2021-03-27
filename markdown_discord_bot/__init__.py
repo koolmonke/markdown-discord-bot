0f7c627e20a63d68af52e12e0360972d6fbe0e93
Artur Khakimov <verybigfolder@yandex.ru>
2021-03-27 16:50:56 +0500

    Preview only with one message. Remove no-preview option

--- markdown_discord_bot/__init__.py
+++ markdown_discord_bot/__init__.py
@@ -8,10 +8,6 @@ DISCORD_MESSAGE_MAX_SIZE = 2000
 client = discord.Client()
 
 
-def split_with_char_limit(string: str, max_size: int):
-    return (string[i:i + max_size] for i in range(0, len(string), max_size))
-
-
 @client.event
 async def on_ready():
     print(f"We have logged in as {client.user}")
@@ -21,21 +17,14 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.attachments and message.content != "no-preview":
+    if message.attachments:
         ext = message.attachments[0].filename.split('.')[-1]
         if ext in config.EXTS:
-            print(f"Got {message.attachments[0].filename} from {message.author}")
             file_content = (await message.attachments[0].read()).decode()
-            is_first = True
             maximum_message_size = DISCORD_MESSAGE_MAX_SIZE - 8 - len(ext)  # 8 - len(ext) is markdown overhead
-            for part in split_with_char_limit(file_content, maximum_message_size):
-                full_message = f"```{ext}\n{part}\n```"
-                print(f"{len(full_message)=}")
-                if is_first:
-                    await message.reply(full_message)
-                    is_first = False
-                else:
-                    await message.channel.send(full_message)
+            full_message = f"```{ext}\n{file_content[:maximum_message_size]}\n```"
+            print(f"Got {message.attachments[0].filename} from {message.author} {len(full_message)=}")
+            await message.reply(full_message)
 
 
 def main():
